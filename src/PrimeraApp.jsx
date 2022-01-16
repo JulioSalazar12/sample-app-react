8be8a005e7674529425bf015566caca083d35506
JulioSalazar12 <u202017572@upc.edu.pe>
2022-01-16 16:34:43 -0500

    feat: created first component

---
+++ src/PrimeraApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PrimeraApp = () => {
+
+    const saludo = 'Primera app';
+
+    return (
+        <>
+        <h1>{ saludo }</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        <p>Consequatur, dolor commodi dolorum odio minima cum nostrum porro illum enim laboriosam.</p>
+        </>
+    )
+}
+
+export default PrimeraApp;
